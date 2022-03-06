97963cf0581c55e65354383f9f743a8d9f8df4a9
Francogalfre <galfre.vn@gmail.com>
2022-03-06 14:58:04 -0300

    If not data return to start

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,30 +1,36 @@
-import React, { useContext } from 'react';
-import FormContext from "../context/FormContext"
+import React, { useContext, useEffect } from "react";
+import FormContext from "../context/FormContext";
+import { useNavigate } from "react-router";
 
 function Show() {
-
-  const { formData } = useContext(FormContext)
+  const { formData } = useContext(FormContext);
   console.log(formData);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!formData.lenght) {
+      navigate("/");
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [formData]);
 
   return (
     <div className="App">
-
-        {formData.one}
-        <br />
-        {formData.two}
-        <br />
-        {formData.three}
-        <br />
-        {formData.four}
-        <br />
-        {formData.four + 20}
-        <br />
-        {formData.five}
-        <br />
-        {formData.six}
-        <br /> 
-        {formData.seven}
-
+      {formData.one}
+      <br />
+      {formData.two}
+      <br />
+      {formData.three}
+      <br />
+      {formData.four}
+      <br />
+      {formData.four + 20}
+      <br />
+      {formData.five}
+      <br />
+      {formData.six}
+      <br />
+      {formData.seven}
     </div>
   );
 }
